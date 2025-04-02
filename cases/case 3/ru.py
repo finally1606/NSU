4eb070897bed02d7ca42eb55d06ce6c65d73c3df
finally1606 <97778943+finally1606@users.noreply.github.com>
2025-04-02 11:50:28 +0700

    Create ru.py

---
+++ cases/case 3/ru.py
@@ -0,0 +1,27 @@
+RESOURCES_TITLE = "Ресурсы группы:"
+FOOD = "Еда: {}"
+WEAPONS = "Оружие: {}"
+MEDICATIONS = "Медикаменты: {}"
+MORALE = "Мораль: {}"
+
+CONTROLLED_EVENT_TITLE = "Управляемое событие! Выберите действие:"
+CONTROLLED_EVENT_OPTION1 = "1. Укрепить базу (стоимость: 20 еды)"
+CONTROLLED_EVENT_OPTION2 = "2. Отправиться на поиски ресурсов"
+CONTROLLED_EVENT_PROMPT = "Ваш выбор: "
+
+BASE_FORTIFIED = "База укреплена!"
+NOT_ENOUGH_FOOD = "Недостаточно еды для укрепления базы!"
+FOUND_FOOD = "Найдена еда: {} единиц!"
+INVALID_CHOICE = "Неверный выбор!"
+
+ZOMBIE_ATTACK_WITH_WEAPONS = "Нападение зомби! Вы отбились, но потеряли {} оружия."
+ZOMBIE_ATTACK_NO_WEAPONS = "Нападение зомби! Вы не смогли защититься и потеряли мораль."
+FOUND_MEDICATIONS = "Найдены медикаменты: {} единиц!"
+
+DAY_TITLE = "=== День {} ==="
+GAME_OVER_TITLE = "=== Игра окончена! ==="
+DAYS_SURVIVED = "Вы продержались {} дней"
+WIN_MESSAGE = "Поздравляем! Группа выжила!"
+LOSE_MESSAGE = "Группа потеряла мораль и не смогла выжить."
+
+PRESS_ENTER = "Нажмите Enter чтобы продолжить..."
