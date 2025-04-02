b9d3bb531febdf3dd4519ea240e9390626c76cb5
finally1606 <97778943+finally1606@users.noreply.github.com>
2025-04-02 14:43:57 +0700

    Update main.py

--- cases/case 3/main.py
+++ cases/case 3/main.py
@@ -10,11 +10,12 @@ days_survived_count = 0
 
 
 def clear_console():
-    #function for cleaning the screen
+    # function for cleaning the screen
     print('\n' * 100)
 
+
 def display_resources():
-    #function to display the resources a player has
+    # function to display the resources a player has
     print(RESOURCES_TITLE)
     print(FOOD.format(food_amount))
     print(WEAPONS.format(weapons_amount))
@@ -24,12 +25,13 @@ def display_resources():
 
 
 def controlled_event():
-    #player action selection function
-    global food_amount, morale_amount
+    # player action selection function
+    global food_amount, morale_amount, medications_amount
 
     print(CONTROLLED_EVENT_TITLE)
     print(CONTROLLED_EVENT_OPTION1)
     print(CONTROLLED_EVENT_OPTION2)
+    print(CONTROLLED_EVENT_OPTION3)
     choice = input(CONTROLLED_EVENT_PROMPT)
 
     if choice == "1":
@@ -45,6 +47,14 @@ def controlled_event():
         food_amount += found_food
         print("\n" + FOUND_FOOD.format(found_food))
 
+    elif choice == "3":
+        if medications_amount >= 1:
+            medications_amount -= 1
+            morale_amount += 30
+            print("\n" + USED_MEDICATIONS)
+        else:
+            print("\n" + NOT_ENOUGH_MEDICATIONS)
+
     else:
         print("\n" + INVALID_CHOICE)
 
@@ -52,11 +62,11 @@ def controlled_event():
 
 
 def uncontrolled_event():
-    #Choose whether the player will be attacked by zombies or find medical supplies
+    # Choose whether the player will be attacked by zombies or find medical supplies
     global weapons_amount, morale_amount, medications_amount
 
     outcome = random.randint(1, 100)
-    if outcome < 40:  # 40% chance to zombie attack
+    if outcome < 50:  # 50% шанс на атаку зомби
         zombies = random.randint(1, 3)
         if weapons_amount > 0:
             weapons_amount -= zombies
